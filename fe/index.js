43abffa146fd0644740c975b3b4830392cd36055
eitanbg666 <eitanbg@ofekschool.org.il>
2026-03-16 14:02:46 +0200

    Add files via upload

---
+++ fe/index.js
@@ -0,0 +1,35 @@
+
+
+
+
+let div = document.querySelector(".main")
+let p = document.querySelector(".main")
+let text = p.textContent;
+console.loglet 
+function func1(){
+    if ( div.style.display ==="none")
+        {
+        div.style.display ="block"
+    }
+    else 
+    {
+        div.style.display ="none"
+    }
+}
+
+function func2(){
+    if (div.Content = "amit levi dior67"){
+      div.textContent = "yoav twito is gay"
+    }
+    else {
+       div.textContent = "amit levi dior67"
+    }
+}
+function addline(){
+    p.style.textDecoration="underline";
+    p.textContent= "new text";
+}
+function change(){
+    p.style.textDecoration="none"
+    p.textContent = text;
+}
